0b218eef632304b7a601c0bd06c4eae163e167bc
lifelonglearnerPM <133527988+lifelonglearnerPM@users.noreply.github.com>
2024-11-21 00:19:11 +0530

    win line logic update

--- script.js
+++ script.js
@@ -40,9 +40,29 @@ const checkWin = () => {
     for (const condition of winningConditions) {
         const [a, b, c] = condition;
         if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            cells[a].classList.add('winning-line', 'horizontal');
-            cells[b].classList.add('winning-line', 'horizontal');
-            cells[c].classList.add('winning-line', 'horizontal');
+            cells[a].classList.add('winning-line');
+            cells[b].classList.add('winning-line');
+            cells[c].classList.add('winning-line');
+
+            // Determine the type of win and add the appropriate class
+            if (a === 0 && b === 1 && c === 2 || a === 3 && b === 4 && c === 5 || a === 6 && b === 7 && c === 8) {
+                cells[a].classList.add('horizontal');
+                cells[b].classList.add('horizontal');
+                cells[c].classList.add('horizontal');
+            } else if (a === 0 && b === 3 && c === 6 || a === 1 && b === 4 && c === 7 || a === 2 && b === 5 && c === 8) {
+                cells[a].classList.add('vertical');
+                cells[b].classList.add('vertical');
+                cells[c].classList.add('vertical');
+            } else if (a === 0 && b === 4 && c === 8) {
+                cells[a].classList.add('diagonal-left-to-right');
+                cells[b].classList.add('diagonal-left-to-right');
+                cells[c].classList.add('diagonal-left-to-right');
+            } else if (a === 2 && b === 4 && c === 6) {
+                cells[a].classList.add('diagonal-right-to-left');
+                cells[b].classList.add('diagonal-right-to-left');
+                cells[c].classList.add('diagonal-right-to-left');
+            }
+
             return true;
         }
     }
