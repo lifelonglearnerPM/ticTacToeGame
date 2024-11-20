d7310aa33d61b4313d2872c79bf42232803f8157
lifelonglearnerPM <133527988+lifelonglearnerPM@users.noreply.github.com>
2024-11-21 00:48:20 +0530

    win cell highlight update

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const statusDisplay = document.getElementById('status');
 const resetButton = document.getElementById('resetBtn');
 
 // Game state variables
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameActive = true;
 
@@ -50,30 +49,10 @@ const checkWin = () => {
     for (const condition of winningConditions) {
         const [a, b, c] = condition;
         if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            // Add the winning line class to the cells
-            cells[a].classList.add('winning-line');
-            cells[b].classList.add('winning-line');
-            cells[c].classList.add('winning-line');
-
-            // Determine the type of win and add the appropriate class
-            if (a === 0 && b === 1 && c === 2 || a === 3 && b === 4 && c === 5 || a === 6 && b === 7 && c === 8) {
-                cells[a].classList.add('horizontal');
-                cells[b].classList.add('horizontal');
-                cells[c].classList.add('horizontal');
-            } else if (a === 0 && b === 3 && c === 6 || a === 1 && b === 4 && c === 7 || a === 2 && b === 5 && c === 8) {
-                cells[a].classList.add('vertical');
-                cells[b].classList.add('vertical');
-                cells[c].classList.add('vertical');
-            } else if (a === 0 && b === 4 && c === 8) {
-                cells[a].classList.add('diagonal-left-to-right');
-                cells[b].classList.add('diagonal-left-to-right');
-                cells[c].classList.add('diagonal-left-to-right');
-            } else if (a === 2 && b === 4 && c === 6) {
-                cells[a].classList.add('diagonal-right-to-left');
-                cells[b].classList.add('diagonal-right-to-left');
-                cells[c].classList.add('diagonal-right-to-left');
-            }
-
+            // Highlight the winning cells
+            cells[a].classList.add('winning-cell');
+            cells[b].classList.add('winning-cell');
+            cells[c].classList.add('winning-cell');
             return true;
         }
     }
@@ -93,6 +72,7 @@ const resetGame = () => {
     statusDisplay.innerText = `Player ${currentPlayer}'s turn`;
     cells.forEach(cell => {
         cell.innerText = '';
+        cell.classList.remove('winning-cell');
     });
 };
 
